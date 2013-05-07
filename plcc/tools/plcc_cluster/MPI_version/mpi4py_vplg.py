afc103118771594a13c174bb3e65ebfd539f3609
Tim Schaefer <ts+code@rcmd.org>
2013-05-07 11:09:36 +0000

    mpi4py python script seems ready now

--- plcc/tools/plcc_cluster/MPI_version/mpi4py_vplg.py
+++ plcc/tools/plcc_cluster/MPI_version/mpi4py_vplg.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
-## script to start vplgWeb and other mass VPLG opertations via openMPI
+## This is the mpi4py script to start vplgWeb and other mass VPLG operations via openMPI.
+## This script is run by the pbs job script, do NOT run it manually.
+## Use 'qsub pbs_start_vplg_via_mpi4py.sh' instead.
+## This script takes as argument a file which holds the full path to the PDB files (one file per line).
 
 from mpi4py import MPI
 from MpiTools import *
 import shlex, subprocess
 import os
 
+print "[MPI4PY] Starting.\n"
+
+print "[MPI4PY] Gathering global MPI information.\n"
 comm_world=MPI.COMM_WORLD
 rank=comm_world.Get_rank()
 pro_count=comm_world.Get_size()
@@ -13,22 +19,30 @@ pro_count=comm_world.Get_size()
 jobs=[]
 result=[]
 
-print "START"
-
+print "[MPI4PY] Preparing to split input data for nodes.\n"
 if(rank==0):
+   print "[MPI4PY] [ROOT] I am responsible for splitting the input data, dividing jobs.\n"
+   # input is a list of paths to PDB files
    input=open(sys.argv[1]).readlines()
+   print "[MPI4PY] [ROOT] There are " + str(len(input)) + " jobs total.\n"
    jobs=data_divide(input,pro_count)
 
+print "[MPI4PY] Getting my part of the job list.\n"
 jobs=comm_world.scatter(jobs,root=0)
-print "["+str(rank)+"]: "+str(len(jobs))+" to do\n"
+print "[MPI4PY] [" + str(rank) + "]: " + str(len(jobs)) + " to do.\n"
 
-### 
-os.chdir("/home/ts/software/plcc_cluster/")
+
+# this script is run in the plcc_cluster/MPI_version directory, so go up to plcc_cluster/
+os.chdir("../")
+
+print "[MPI4PY] Running script 'process_single_pdb_file.sh' for all " + str(len(jobs)) + " jobs (PDB files) in my list.\n"
+
 for cur_job in jobs:
       ### <paralell operation> ###
 #      if ((cur_job%7)==0):
 #         result.append(cur_job)
       pdbfile=cur_job      
+      # Note: the PLCC command line options are set in the settings_statistics.cfg file
       command_line="./process_single_pdb_file.sh " + pdbfile + ""
       args = shlex.split(command_line)
       p = subprocess.Popen(args)
