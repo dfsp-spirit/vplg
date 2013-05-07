68f1bd2de348ca2853d96e496cc91ba706938bf4
Tim Schaefer <ts+code@rcmd.org>
2013-05-07 21:08:23 +0000

    minor doc changes

--- plcc/src/resources/Resources.java
+++ plcc/src/resources/Resources.java
@@ -1,12 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of the Visualization of Protein Ligand Graphs (VPLG) software package.
+ *
+ * Copyright Tim Schäfer 2012. VPLG is free software, see the LICENSE and README files for details.
+ *
+ * @author ts
  */
 package resources;
 
 /**
- *
- * @author spirit
+ * This is only here to access the resources.
+ * @author ts
  */
 public class Resources {
     
