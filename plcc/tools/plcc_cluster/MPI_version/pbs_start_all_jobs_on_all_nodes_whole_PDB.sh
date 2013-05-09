d95e8cf3a096e4b668012305a42097cfb4e44a62
Tim Schaefer <ts+code@rcmd.org>
2013-05-09 12:17:51 +0000

    removed file that was accidentely added

--- plcc/tools/plcc_cluster/MPI_version/pbs_start_all_jobs_on_all_nodes_whole_PDB.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# start_all_jobs_on_all_nodes_for_whole_PDB.sh -- submit the whole PDB to the pbs queue
-# 
-# Run this script from the shell after preparing the MPI-version of the PDB file lists.
-#
-# This script does not require any parameters, so just do: './pbs_start_all_jobs_on_all_nodes_whole_PDB.sh'
-#
-# written by ts, 2013
-#
