108ba05e31dd5bf479d8b538bf2ca0c6f893070a
Tim Schaefer <ts+code@rcmd.org>
2013-05-11 15:08:19 +0000

    work on wrapper pbs script

--- plcc/tools/plcc_cluster/MPI_version/start_all_jobs_on_all_nodes_whole_PDB.sh
+++ plcc/tools/plcc_cluster/MPI_version/start_all_jobs_on_all_nodes_whole_PDB.sh
@@ -7,3 +7,23 @@
 #
 # written by ts, 2013
 #
+
+APPTAG="[START_ALL_MPI]"
+
+## settings
+SINGLE_JOB_SCRIPT="pbs_start_vplg_via_mpi4py_jpbs_for_one_node.sh"
+
+echo "$APPTAG Preparing to submit all jobs to the openbpbs queue..."
+
+## submit a job for each file list
+for FLIST in MPIfilelistnum*;
+do
+  ENV_VARIABLES="PDBFILELIST=$FLIST"
+  CMD="qsub $SINGLE_JOB_SCRIPT -v $ENV_VARIABLES"
+  echo "$APPTAG Submitting job: '$CMD'"
+  $CMD
+done
+
+echo "$APPTAG Submitted all jobs. Exiting."
+
+exit
