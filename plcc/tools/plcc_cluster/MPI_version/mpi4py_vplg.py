3d512e6e8675458f4041d512b22c04956ad18a8e
Tim Schaefer <ts+code@rcmd.org>
2013-05-21 10:25:26 +0000

    fixed error in vplg MPI script which made it start to many jobs at once

--- plcc/tools/plcc_cluster/MPI_version/mpi4py_vplg.py
+++ plcc/tools/plcc_cluster/MPI_version/mpi4py_vplg.py
@@ -4,15 +4,18 @@
 ## Use 'qsub pbs_start_vplg_via_mpi4py.sh' instead.
 ## This script takes as argument a file which holds the full path to the PDB files (one file per line).
 
-print "[MPI4PY] Starting.\n"
+
+apptag = "[MPI4PY] "
+
+print apptag + "Starting.\n"
 
 import sys
 import os
-print "[MPI4PY] PATH is '" + str(sys.path) + "'.\n"
+print apptag + "PATH is '" + str(sys.path) + "'.\n"
 curpath=os.getcwd()
 sys.path.append(str(curpath + '/../../mpi4py/src/MPI/'))
 sys.path.append(str(curpath + '/../../mpi4py/build/lib.linux-x86_64-2.6/mpi4py/include/'))
-print "[MPI4PY] Appended copied MPI dirs to PATH. PATH is now '" + str(sys.path) + "'.\n"
+print apptag + "Appended copied MPI dirs to PATH. PATH is now '" + str(sys.path) + "'.\n"
 from mpi4py import MPI
 from mpi4py_tools import *
 import shlex, subprocess
@@ -20,31 +23,32 @@ import os
 
 
 
-print "[MPI4PY] Gathering global MPI information.\n"
+print apptag + "Gathering global MPI information.\n"
 comm_world=MPI.COMM_WORLD
 rank=comm_world.Get_rank()
+apptag = "[MPI4PY] [" + str(rank) + "] "
 pro_count=comm_world.Get_size()
 
 jobs=[]
 result=[]
 
-print "[MPI4PY] Preparing to split input data for nodes.\n"
+print apptag + "Preparing to split input data for nodes.\n"
 if(rank==0):
-   print "[MPI4PY] [ROOT] I am responsible for splitting the input data, dividing jobs.\n"
+   print apptag + "[ROOT] I am responsible for splitting the input data, dividing jobs.\n"
    # input is a list of paths to PDB files
    input=open(sys.argv[1]).readlines()
-   print "[MPI4PY] [ROOT] There are " + str(len(input)) + " jobs total.\n"
+   print apptag + "[ROOT] There are " + str(len(input)) + " jobs total.\n"
    jobs=data_divide(input,pro_count)
 
-print "[MPI4PY] Getting my part of the job list.\n"
+print apptag + "Getting my part of the job list.\n"
 jobs=comm_world.scatter(jobs,root=0)
-print "[MPI4PY] [" + str(rank) + "]: " + str(len(jobs)) + " to do.\n"
+print apptag + "There are " + str(len(jobs)) + " to do for me.\n"
 
 
 # this script is run in the plcc_cluster/MPI_version directory, so go up to plcc_cluster/
 os.chdir("../")
 
-print "[MPI4PY] Running script 'process_single_pdb_file.sh' for all " + str(len(jobs)) + " jobs (PDB files) in my list.\n"
+print apptag + "Running script 'process_single_pdb_file.sh' for all " + str(len(jobs)) + " jobs (PDB files) in my list.\n"
 
 for cur_job in jobs:
       ### <paralell operation> ###
@@ -53,11 +57,15 @@ for cur_job in jobs:
       pdbfile=cur_job      
       # Note: the PLCC command line options are set in the settings_statistics.cfg file
       command_line="./process_single_pdb_file.sh " + pdbfile + ""
+      print apptag + "  Running command '" + command_line + "'.\n"
       args = shlex.split(command_line)
       p = subprocess.Popen(args)
-      #exitCode = p.wait()
+      retVal = p.wait()
+      print apptag + "  Done, command '" + command_line + "' returned exit code " + str(retVal) + ".\n"
       ### </paralell operation> ###
 ### collect all results in rootprocess - save/handle results:
+
+print apptag + "All my jobs are done."
 
 #all_result=comm_world.gather(result,root=0)
 #if(rank==0):
