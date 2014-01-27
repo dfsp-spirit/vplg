d12cef769d890d4e6cb2009a2b18aa61cae77ac3
Andreas Scheck <graziano.bud+github@gmail.com>
2014-01-27 14:42:06 +0000

    added new PTGL web interface by Andreas

---
+++ web/PTGL3/bootstrap/js/script.js
@@ -0,0 +1,31 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$(function() {
+	
+	$('#alertMe').click(function(e) {
+		
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+		
+	});
+	
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+	
+	$('a.pop').popover();
+	
+	$('[rel="tooltip"]').tooltip();
+	
+	
+});
