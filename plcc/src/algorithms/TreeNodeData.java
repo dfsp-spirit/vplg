c46a03c7acce1f82387ed0e2156a3da54bf935da
Tim Schaefer <ts+code@rcmd.org>
2014-07-08 20:45:21 +0000

    added missing TreeNodeData class for BK recursion tree visualization

---
+++ plcc/src/algorithms/TreeNodeData.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the Visualization of Protein Ligand Graphs (VPLG) software package.
+ *
+ * Copyright Tim Schäfer 2012. VPLG is free software, see the LICENSE and README files for details.
+ *
+ * @author ts
+ */
+
+package algorithms;
+
+/**
+ *
+ * @author spirit
+ */
+public class TreeNodeData {
+    
+    public String label;
+    public String cliqueString;
+    boolean markedSpecial;
+    
+    public TreeNodeData(String s) {
+        label = s;
+        cliqueString = "";
+        markedSpecial = false;
+    }
+    
+    public void markSpecial(boolean s) {
+        this.markedSpecial = s;
+    }
+    
+    public void markSpecial(boolean s, String info) {
+        this.markedSpecial = s;
+        this.cliqueString = info;
+    }
+    
+    @Override public String toString() {
+        if(this.markedSpecial) {
+            return "(" + label + ")" + cliqueString;
+        }
+        return label;
+    }
+    
+}
