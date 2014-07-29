0a3df17cbde0853f71d59c9f3f1a7fd742a66867
Tim Schaefer <ts+code@rcmd.org>
2014-07-29 12:20:58 +0000

    added cron script to rsync results

---
+++ plcc/tools/cron_run_unless_running/run_unless_running.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+### run_unless_running.sh -- Sync the contents of two directories via rsync unless the old cron rsync process still runs ###
+### by Tim Schaefer
+### This script is to be run by cron while the PTGL update is in progress. ###
+### Add a cronjob by running 'crontab -e', then edit the file. An example is given below. Be sure to adapt the time to your command. ###
+### Example crontab line to run this every 3 hours: 
+###
+###      '0 */3 * * * /path/to/run_unless_running.sh'
+###
+### Make sure to edit the settings below to fill in your command. And make sure that cron is running, of course. ;) That's it.
+
+
+# some commands we need
+KILL="/bin/kill"
+ECHO="/bin/echo"
+RM="/bin/rm"
+DATE="/bin/date"
+CAT="/bin/cat"
+
+DATESTR=$($DATE)
+APPTAG="[run_unless_running]"
+$ECHO "$APPTAG Checking whether to run commands. Current time is $DATESTR."
+
+########## settings -- edit these ##############
+
+# The process ID file. You only need to change this if you run this script in parallel for different commands on the same server.
+PIDFILE=/var/run/rur_vplg-copy-results.pid
+
+# The command that should be executed (unless it still runs). This most likely takes some time.
+COMMAND="/usr/bin/rsync -avh source target"
+
+
+########## end of settings -- no need to edit below this line ##########
+
+DATESTR=$($DATE)
+
+if [ -e "$PIDFILE" ] ; then
+    # our pidfile exists, let's make sure the process is still running though
+    PID=`$CAT "$PIDFILE"`
+    if $KILL -0 "$PID" > /dev/null 2>&1 ; then
+        $ECHO "$APPTAG The script is still running, skipping this run at $DATESTR."
+        exit 0
+    fi
+ fi
+
+# create or update the pidfile
+$ECHO "$$" > $PIDFILE
+
+# run the command we came for
+$($COMMAND)
+
+# remove PID file
+$RM -f "$PIDFILE"
+
+$ECHO "$APPTAG Exiting."
