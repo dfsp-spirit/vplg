108699c451cc2d2de6a28872c027bd9399d79939
Tim Schaefer <ts+code@rcmd.org>
2014-08-04 13:55:11 +0000

    fixed thumbnail function

--- web/PTGL3/backend/getThumbImages.php
+++ web/PTGL3/backend/getThumbImages.php
@@ -17,6 +17,60 @@
  * @author Daniel Bruness <dbruness@gmail.com>
  * @author Andreas Scheck <andreas.scheck.home@googlemail.com>
  */
+ 
+function construct_png_file_name_of($pdbid, $chain, $graphtype) {
+  $valid_pdbid = FALSE;
+  $valid_chain = FALSE;
+  $valid_graphtype = TRUE; // TODO: check this
+  
+  $base_data_dir = "./data";
+  
+  if(ctype_alnum($pdbid) && strlen($pdbid) == 4) { $valid_pdbid = TRUE; }
+  if(ctype_alnum($chain) && strlen($chain) == 1) { $valid_chain = TRUE; }
+  
+  if($valid_pdbid) {
+    $mid_chars = substr($pdbid, 1, 2);
+    if($valid_chain) {
+        $link = $base_data_dir . "/" . $mid_chars . "/" . $pdbid . "/" . $chain . "/";
+    }
+    else {
+        $link = $base_data_dir . "/" . $mid_chars . "/" . $pdbid . "/";
+    }
+    
+    if (file_exists($link)) {
+        if ($valid_chain) {
+            $link .= $chain . "/" . $pdbid . '_' . $chain . '_' . $graphtype . '_PG.png';
+            if(file_exists($link)) {
+	      return $link;
+            }
+            else {
+	      return ""; // image not found on disk
+            }
+        }
+        else {
+            return "";	// invalid chain
+        }
+    }
+    else {
+        return "";	// no pdb and chain dir found on disk
+    }
+  }
+  else { 
+    return ""; // invalid pdb id
+  }
+}
+
+/**
+  * Returns a relative file system path to an image file or the empty string if no path could be determined / the file was missing on disk.
+  */
+function get_valid_png_file_name_on_disk_of($pdbid, $chain, $graphtype) {
+  //return get_png_file_name_from_database_of($pdbid, $chain, $graphtype);	//TODO: implement and use this one!
+  return construct_png_file_name_of($pdbid, $chain, $graphtype);
+}
+
+function get_png_file_name_from_database_of($pdbid, $chain, $graphtype) {
+  die("get_png_file_name_from_database_of(): Daniel, please implement me!!!11!11!\n");
+}
 
 $graphtype = "alpha"; # alpha-helix is #1
 $graphtypes = array("alpha", "beta", "albe", "alphalig", "betalig", "albelig");
@@ -52,10 +106,13 @@ $output = '<p>- Select topology type -</p>
 
 $c = 1;					
 foreach ($graphtypes as $gt){
-	$output .= ' <a class="thumbalign" data-slide-index="'.$c++.'" href=""><img src="./data/'.$pdbID.'_'.$chainName.'_'.$gt.'_PG.png" width="100px" height="100px" />
+        $gt_file = get_valid_png_file_name_on_disk_of($pdbID, $chainName, $gt);	// also ensures that the file exists on disk!
+        if(strlen($gt_file) > 0) {	
+	  $output .= ' <a class="thumbalign" data-slide-index="'.$c++.'" href=""><img src="./data/'. $gt_file . ' width="100px" height="100px" />
 						'.$graphtype_dict[$gt].'
 					  </a>
 					';
+	}
 }
 
 echo $output;
