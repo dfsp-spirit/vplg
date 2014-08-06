19a4dcf393d2c5024d679ccf175e659d96901769
Tim Schaefer <ts+code@rcmd.org>
2014-08-06 15:17:12 +0000

    work on plots in content.php. Should ne enough.

--- web/PTGL3/content.php
+++ web/PTGL3/content.php
@@ -61,22 +61,110 @@
 
   // creates a bar chart
   function do_plot_js_bar($context_name = "ctx", $dataset_var_name = "data", $result_assign_var_name = "myChart") {
-    $res = 'var ' . $result_assign_var_name . ' = new Chart(' . $context_name . ').Bar(' . $dataset_var_name . ');' . "\n";
+    $res = 'var ' . $result_assign_var_name . ' = new Chart(' . $context_name . ').Bar(' . $dataset_var_name . ', { scaleBeginAtZero : true, scaleStartValue: 0 }' . ');' . "\n";
     return $res;
   }
   
   // creates a pe chart
   function do_plot_js_pie($context_name = "ctx", $dataset_var_name = "data", $result_assign_var_name = "myChart") {
-    $res = 'var ' . $result_assign_var_name . ' = new Chart(' . $context_name . ').Pie(' . $dataset_var_name . ');' . "\n";
+    $res = 'var ' . $result_assign_var_name . ' = new Chart(' . $context_name . ').Pie(' . $dataset_var_name . ', { tooltipTemplate : "<%if (label){%><%=label%>: <%}%><%= value %> f", animation: false }' . ');' . "\n";
+    //$res .= 'var legend = ' . $result_assign_var_name . '.generateLegend();' . "\n";
+    //$res .= '$("#legend").html(legend);' . "\n";
     return $res;
   }
   
   // creates a doughnut chart
   function do_plot_js_doughnut($context_name = "ctx", $dataset_var_name = "data", $result_assign_var_name = "myChart") {
-    $res = 'var ' . $result_assign_var_name . ' = new Chart(' . $context_name . ').Doughnut(' . $dataset_var_name . ');' . "\n";
+    $res = 'var ' . $result_assign_var_name . ' = new Chart(' . $context_name . ').Doughnut(' . $dataset_var_name . ', { tooltipTemplate : "<%if (label){%><%=label%>: <%}%><%= value %> f", animation: false }' . ');' . "\n";
+    //$res .= 'var legend = ' . $result_assign_var_name . '.generateLegend();' . "\n";
+    //$res .= '$("#legend").html(legend);' . "\n";
     return $res;
   }
   
+  /*
+  Returns something like this (see http://www.chartjs.org/docs/#doughnut-pie-chart-introduction):
+  
+  var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
+
+  */
+  function get_plot_js_formatted_data_pie_doughnut($labels = array(), $data = array(), $colors = array("#F7464A", "#46BFBD" , "#FDB45C"), $highlights = array("#FF5A5E", "#5AD3D1" , "#FFC870")) {
+    $res = "[" . "\n";     // open var name
+
+
+    for($i = 0; $i < count($data); $i++) {
+      $res .= "{\n";
+      $res .= "value: " . $data[$i] . ",\n";
+      
+      $color_index = $i;
+      if($color_index >= count($colors)) {
+        $color_index = $color_index % count($colors);
+      }
+      
+      $highlight_index = $i;
+      if($highlight_index >= count($highlights)) {
+        $highlight_index = $highlight_index % count($highlights);
+      }
+      
+      $res .= "color: " . '"' . $colors[$color_index] . '"' . ",\n";
+      $res .= "highlight: " . '"' . $highlights[$highlight_index] . '"' . ",\n";
+      $res .= "label :" . '"' . $labels[$i] . '"' . "\n";
+      $res .= "}\n";
+      
+      if($i < (count($data) - 1)) {
+	$res .= ',';
+      }
+    }
+    $res .= "]" . "\n";     // close everything
+
+    return $res;
+  }
+  
+  
+  /*
+  Returns something like this (see http://www.chartjs.org/docs/#bar-chart-introduction):
+  
+  
+  var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};  
+  */
   function get_plot_js_formatted_data($labels = array(), $datasets = array()) {
   
     $format_commands = array();
@@ -172,22 +260,27 @@
     
     $ret_code .= "<script>\n";
     $ret_code .= "var " . $data_name . " = ";
-    $ret_code .= get_plot_js_formatted_data($labels, $datasets);
+    
     
     // start the actual plot
     if($plot_type == "bar") {
+      $ret_code .= get_plot_js_formatted_data($labels, $datasets);
       $ret_code .= do_plot_js_bar($ctx_name, $data_name, $chart_name);
     }
     else if($plot_type == "pie") {
+      $ret_code .= get_plot_js_formatted_data_pie_doughnut($labels, $datasets[0]);
       $ret_code .= do_plot_js_pie($ctx_name, $data_name, $chart_name);
     }
     else if($plot_type == "doughnut") {
+      $ret_code .= get_plot_js_formatted_data_pie_doughnut($labels, $datasets[0]);
       $ret_code .= do_plot_js_doughnut($ctx_name, $data_name, $chart_name);
     }
     else if($plot_type == "line") {
+      $ret_code .= get_plot_js_formatted_data($labels, $datasets);
       $ret_code .= do_plot_js_line($ctx_name, $data_name, $chart_name);
     }
     else {	// assume "line"
+      $ret_code .= get_plot_js_formatted_data($labels, $datasets);
       $ret_code .= do_plot_js_line($ctx_name, $data_name, $chart_name);
     }
     $ret_code .= "</script>\n";
@@ -196,6 +289,27 @@
   }
 
 ?>
+
+
+<?php
+
+  $table_data = array();    
+  $row_alpha = array("Alpha", 247454, 0, 2357932);
+  $row_beta = array("Beta", 247452, 169500, 2094599);
+  $row_albe = array("Alpha-Beta", 247459, 169542, 4452531);
+  $row_alphalig = array("Alphalig", 247454, 0, 2988890);
+  $row_betalig = array("Betalig", 247452, 169560, 2723377);
+  $row_albelig = array("Alpha-Betalig", 247459, 169542, 5099843);
+  
+  array_push($table_data, $row_alpha);
+  array_push($table_data, $row_beta);
+  array_push($table_data, $row_albe);
+  array_push($table_data, $row_alphalig);
+  array_push($table_data, $row_betalig);
+  array_push($table_data, $row_albelig);
+
+?>
+
 
 <body id="customBackground">
 	<noscript>
@@ -224,64 +338,57 @@
 					<th class="tablecenter">Number of SSEs in graphs</th>
 				</tr>
 				
+				<!--
 				<tr class="tablecenter">
 					<th>Alpha</th>
 					<td>274,454</td>
 					<td>0</td>
 					<td>2,357,932</td>
-
 				</tr>
+				-->			
+	
 				
-				<tr class="tablecenter">
-					<th>Beta</th>
-					<td>274,452</td>
-					<td>169,560</td>
-					<td>2,094,599</td>
-				</tr>
-				
-				<tr class="tablecenter">
-					<th>Alpha-Beta</th>
-					<td>274,459</td>
-					<td>169,542</td>
-					<td>4,452,531</td>
-				</tr>
-				
-				<tr class="tablecenter">
-					<th>Alphalig</th>
-					<td>274,454</td>
-					<td>0</td>
-					<td>2,988,890</td>
-
-				</tr>
-				
-				<tr class="tablecenter">
-					<th>Betalig</th>
-					<td>274,452</td>
-					<td>169,560</td>
-					<td>2,723,377</td>
-				</tr>
-				
-				<tr class="tablecenter">
-					<th>Alpha-Betalig</th>
-					<td>274,459</td>
-					<td>169,542</td>
-					<td>5,099,843</td>
-				</tr>
+				<?php
+				  for($i = 0; $i < sizeof($table_data); $i++) {
+				     echo '<tr class="tablecenter">' . "\n";
+				     for($j = 0; $j < sizeof($table_data[$i]); $j++) {
+				       if($j == 0) {
+				         echo '<th>' . $table_data[$i][$j] . '</th>' . "\n";
+				       } else {
+				         echo '<td>' . $table_data[$i][$j] . '</td>' . "\n";
+				       }
+				     }
+				     echo '</tr>' . "\n\n";
+				  }
+				?>
 				
 			</table>
 		</div><!-- end table-responsive -->
 		
 <?php
 		
-		  $labels = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");        // mysql order weekdays
+		  $labels = array("Alpha", "Beta", "Alpha-Beta", "Alphalig", "Betalig", "Alpha-Betalig");
 		  $datasets = array();
-		  $dataset1 = array(11, 23, 3, 5, 24, 44, 7);
+		  $dataset1 = array($table_data[0][3], $table_data[1][3], $table_data[2][3], $table_data[3][3], $table_data[4][3], $table_data[5][3]);
 		  array_push($datasets, $dataset1);
 		  $plot_type = "bar"; // "bar", "pie", "doughnut" or "line" are supported atm
 		  //$plot_type = "pie";
 		  
 		  $code = get_plot_code("1", $plot_type, $labels, $datasets);		  
-		  //echo $code;
+		  
+		  echo "<h4>Number of SSEs in graphs</h4>\n" . $code . "\n\n";
+		  
+		  echo "<br/><br/><br/><br/>\n";
+		  
+		  $labels = array("Helix", "Strand", "Ligand");
+		  $datasets = array();
+		  $dataset1 = array(2357932, 2094599, 636312);
+		  array_push($datasets, $dataset1);
+		  
+		  $plot_type = "bar";		  
+		  $code = get_plot_code("2", $plot_type, $labels, $datasets);		  
+		  
+		  echo "<h4>Number of SSEs by type</h4>\n" . $code . "\n\n";
 		  
 ?>
 		
