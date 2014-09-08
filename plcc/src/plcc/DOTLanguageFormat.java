e93a4432d2e3afd4bb139cdc79184da31a75d8de
Tim Schaefer <ts+code@rcmd.org>
2014-09-08 12:22:30 +0000

    Added new views for the PTGL linear notations of the folding graphs

--- plcc/src/plcc/DOTLanguageFormat.java
+++ plcc/src/plcc/DOTLanguageFormat.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * This file is part of the Visualization of Protein Ligand Graphs (VPLG) software package.
  *
  * Copyright Tim Schäfer 2012. VPLG is free software, see the LICENSE and README files for details.
