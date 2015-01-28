9ebceb5aeefb64f0ace7f728cadf000e1623c5b3
Tim Schaefer <ts+code@rcmd.org>
2015-01-28 16:40:25 +0000

    added file to hold stats data

---
+++ web/PTGL3/temp_data/content_data.php
@@ -0,0 +1,29 @@
+<?php
+    $last_update_month_and_year = "January 2015";
+      
+    $num_pdb_files = 98161;
+    $num_protein_chains = 274459;
+    $num_sses = 5088843;
+    $num_intrachain_contacts = 4964550;
+    
+    $sses_in_graphtypes_labels = array("Alpha", "Beta", "Alpha-Beta", "Alphalig", "Betalig", "Alpha-Betalig");
+    $sses_in_graphtype_alpha = array("Alpha", "247454", "0", "2357932");
+    $sses_in_graphtype_beta = array("Beta", 247452, 169500, 2094599);
+    $sses_in_graphtype_albe = array("Alpha-Beta", 247459, 169542, 4452531);
+    $sses_in_graphtype_alphalig = array("Alphalig", 247454, 0, 2988890);
+    $sses_in_graphtype_betalig = array("Betalig", 247452, 169560, 2723377);
+    $sses_in_graphtype_albelig = array("Alpha-Betalig", 247459, 169542, 5099843);
+  
+    
+    $sses_by_type_labels = array("Helix", "Strand", "Ligand");
+    $sses_by_type = array(2357932, 2094599, 636312);
+    
+    $total_contacts_by_type_labels = array("Mixed", "Parallel", "Antiparallel", "Ligand");
+    $total_contacts_by_type = array(1232714, 776965, 1553370, 1391501);
+    
+    $protein_functions_labels = array("Transcription", "Unknown", "Immune system", "Lyase", "Oxidureductase", "Transferase", "Hydrolase");
+    $protein_functions = array(2302, 2394, 2609, 3257, 8892, 11330, 14696);
+    
+    $source_organisms_labels = array("Bos taurus", "Saccharomyces cerevisia", "Mus musculus", "Thermus thermophilus", "Unknown", "Escherichia coli", "Homo sapiens");
+    $source_organisms = array(5219, 8822, 9349, 11464, 19161, 21117, 53667);
+?>
