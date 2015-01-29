f7c5081b9bdd5a90a9a031afa6f959e964d6be4a
Tim Schaefer <ts+code@rcmd.org>
2015-01-29 09:16:01 +0000

    updated SQL stats query scripts

--- plcc/tools/plcc_cluster/query_scripts/get_all_stats.sh
+++ plcc/tools/plcc_cluster/query_scripts/get_all_stats.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+echo "Running all query scripts, this may take a sec depending on database size..."
 
 for SCRIPT in q_overview.sh q_sselengths.sh q_functions.sh q_contacts.sh q_contact_details.sh q_organisms.sh q_graphtypes.sh q_motifs.sh
 do
-	echo "Handling script '$SCRIPT'..."
-	./$SCRIPT > ./results/result_${SCRIPT}
+	echo " *Handling script '$SCRIPT'..."
+	SCRIPT_NO_EXT=${SCRIPT%.*}
+	RESULT_FILE="./results/result_${SCRIPT_NO_EXT}.txt"
+	./$SCRIPT > $RESULT_FILE
+	echo "  Results of script '$SCRIPT' written to '$RESULT_FILE'."
 done
 
 
-echo "Done."
+echo "All done."
 exit 0
 
