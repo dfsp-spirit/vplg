075b5c329c1eac88d98b7b483d4c50f000e033ae
Daniel Bruness <dbruness@gmail.com>
2015-02-24 15:11:37 +0000

    graphDegreeDistribution considers 0-degree nodes now

--- web/PTGL3/getGraphDegreeDistribution.php
+++ web/PTGL3/getGraphDegreeDistribution.php
@@ -32,7 +32,9 @@ if(isset($_GET["csv"])){
 $conn_string = "host=" . $DB_HOST . " port=" . $DB_PORT . " dbname=" . $DB_NAME . " user=" . $DB_USER . " password=" . $DB_PASSWORD;
 $db = pg_connect($conn_string);
 
-
+//QUERYMANIA
+// Select the maximum degree, the count of the 0-degree and the remaining degrees for
+// each contact types (SSE interchain contacts, SSE complex contacts and chain complex contacts)
 
 $query_contact_max = "SELECT MAX(y.count) 
                     FROM 
@@ -68,6 +70,29 @@ $query_chain_complex_max = "SELECT MAX(y.count)
                         GROUP BY chain) y";
 
 
+$query_contact_nulldeg = "SELECT COUNT(s.sse_id) 
+                                FROM   plcc_sse s
+                                WHERE s.sse_id NOT IN (SELECT sse1 as sse 
+                                                        FROM plcc_contact
+                                                        UNION
+                                                        SELECT sse2 as sse 
+                                                        FROM plcc_contact)";
+
+$query_sse_complex_nulldeg = "SELECT COUNT(s.sse_id) 
+                                FROM   plcc_sse s
+                                WHERE s.sse_id NOT IN (SELECT sse1 as sse 
+                                                        FROM plcc_ssecontact_complexgraph
+                                                        UNION
+                                                        SELECT sse2 as sse 
+                                                        FROM plcc_ssecontact_complexgraph)";
+
+$query_chain_complex_nulldeg = "SELECT COUNT(c.chain_id) 
+                                FROM   plcc_chain c
+                                WHERE c.chain_id NOT IN (SELECT chain1 as chain 
+                                                        FROM plcc_complex_contact
+                                                        UNION
+                                                        SELECT chain2 as chain 
+                                                        FROM plcc_complex_contact)";
 
 $query_contact = "SELECT c.sse1 as sse, COUNT(c.sse1) as count
                     FROM plcc_contact c
@@ -99,6 +124,7 @@ $query_chain_complex = "SELECT c.chain1 as chain, COUNT(c.chain1) as count
                         GROUP BY chain
                       ORDER BY count DESC";
 
+// GET MAX DEGREE
 $res_contact_max = pg_query($db, $query_contact_max);
 $contact_max_deg = pg_fetch_array($res_contact_max, NULL, PGSQL_ASSOC);
 $contact_max_deg = $contact_max_deg["max"];
@@ -109,12 +135,31 @@ $sse_complex_max_deg = $sse_complex_max_deg["max"];
 
 $res_chain_complex_max = pg_query($db, $query_chain_complex_max);
 $chain_complex_max_deg = pg_fetch_array($res_chain_complex_max, NULL, PGSQL_ASSOC);
-$chain_complex_max_deg = $chain_complex_max_deg["max"]; 
+$chain_complex_max_deg = $chain_complex_max_deg["max"];
 
+
+// GET DEGREES OF ZERO
+$res_contact_nulldeg = pg_query($db, $query_contact_nulldeg);
+$contact_nulldeg = pg_fetch_array($res_contact_nulldeg, NULL, PGSQL_ASSOC);
+$contact_nulldeg = $contact_nulldeg["count"];
+
+$res_sse_complex_nulldeg = pg_query($db, $query_sse_complex_nulldeg);
+$sse_complex_nulldeg = pg_fetch_array($res_sse_complex_nulldeg, NULL, PGSQL_ASSOC);
+$sse_complex_nulldeg = $sse_complex_nulldeg["count"];
+
+$res_chain_complex_nulldeg = pg_query($db, $query_chain_complex_nulldeg);
+$chain_complex_nulldeg = pg_fetch_array($res_chain_complex_nulldeg, NULL, PGSQL_ASSOC);
+$chain_complex_nulldeg = $chain_complex_nulldeg["count"];
+
+// GET remaining degrees
+$res_contact = pg_query($db, $query_contact);
+$res_sse_complex = pg_query($db, $query_sse_complex);
+$res_chain_complex = pg_query($db, $query_chain_complex);
+
+// generate arrays for "remaining" degrees
 $dist_contact = array();
 $dist_sse_complex = array();
 $dist_chain_complex = array();
-
 
 for($i = 1; $i <= $contact_max_deg; $i++){
     $dist_contact[$i] = 0;
@@ -127,11 +172,6 @@ for($i = 1; $i <= $sse_complex_max_deg; $i++){
 for($i = 1; $i <= $chain_complex_max_deg; $i++){
     $dist_chain_complex[$i] = 0;
 }
-
-
-$res_contact = pg_query($db, $query_contact);
-$res_sse_complex = pg_query($db, $query_sse_complex);
-$res_chain_complex = pg_query($db, $query_chain_complex);
 
 
 while ($arr = pg_fetch_array($res_contact, NULL, PGSQL_ASSOC)) {
@@ -149,62 +189,78 @@ while ($arr = pg_fetch_array($res_chain_complex, NULL, PGSQL_ASSOC)) {
     $dist_chain_complex[$degree]++;
 }
 
+
 pg_close($db);
 
-
+// just to shorten code a bit
+// calculate percentage
 function perc($sum, $part) {
     return round(($part/($sum/100)),3);
 }
 
-echo "SSE contact distribution\n";
+// sums of 0-degrees and remaining degrees
+$contact_sum = (array_sum($dist_contact) + $contact_nulldeg);
+$sse_complex_sum = (array_sum($dist_sse_complex) + $sse_complex_nulldeg);
+$chain_complex_sum = (array_sum($dist_chain_complex) + $chain_complex_nulldeg);
+
+
+echo "// SSE contact distribution\n";
 if($asCSV){
     echo "Degree;Count;Percentage\n";
+    echo "0;" . $contact_nulldeg . ";" . perc($contact_sum, $contact_nulldeg)  . "\n";
     for($i = 1; $i <= count($dist_contact); $i++ ){
-        echo "" . $i . ";" . $dist_contact[$i] . ";" . perc(array_sum($dist_contact), $dist_contact[$i]) . "\n";
-    }    
+        echo "" . $i . ";" . $dist_contact[$i] . ";" . perc($contact_sum, $dist_contact[$i]) . "\n";
+    }
+    echo "SUM;" . $contact_sum . ";100\n";
 } else {
     echo "Degree\tCount\tPercentage\n";
+    echo "0\t" . $contact_nulldeg . "\t" . perc($contact_sum, $contact_nulldeg) . "\n";
     for($i = 1; $i <= count($dist_contact); $i++ ){
-        echo "" . $i . "\t" . $dist_contact[$i] . "\t" . perc(array_sum($dist_contact), $dist_contact[$i]) . "\n";
+        echo "" . $i . "\t" . $dist_contact[$i] . "\t" . perc($contact_sum, $dist_contact[$i]) . "\n";
     }
-
+    echo "\n";
+    echo "SUM\t" . $contact_sum . "\t100\n";
     echo "-----------------------end";
 }
 echo "\n\n\n";
 
 
-echo "SSE complex contact distribution\n";
+echo "// SSE complex contact distribution\n";
 if($asCSV){
     echo "Degree;Count;Percentage\n";
+    echo "0;" . $sse_complex_nulldeg . ";" . perc($sse_complex_sum, $sse_complex_nulldeg)  . "\n";
     for($i = 1; $i <= count($dist_sse_complex); $i++ ){
-        echo "" . $i . ";" . $dist_sse_complex[$i] . ";" . perc(array_sum($dist_sse_complex), $dist_sse_complex[$i]) .  "\n";
-    }    
+        echo "" . $i . ";" . $dist_sse_complex[$i] . ";" . perc($sse_complex_sum, $dist_sse_complex[$i]) .  "\n";
+    }
+    echo "SUM;" . $sse_complex_sum . ";100\n";
 } else {
     echo "Degree\tCount\tPercentage\n";
+    echo "0\t" . $sse_complex_nulldeg . "\t" . perc($sse_complex_sum, $sse_complex_nulldeg) . "\n";
     for($i = 1; $i <= count($dist_sse_complex); $i++ ){
-        echo "" . $i . "\t" . $dist_sse_complex[$i] . "\t" . perc(array_sum($dist_sse_complex), $dist_sse_complex[$i]) . "\n";
+        echo "" . $i . "\t" . $dist_sse_complex[$i] . "\t" . perc($sse_complex_sum, $dist_sse_complex[$i]) . "\n";
     }
-
+    echo "\n";
+    echo "SUM\t" . $sse_complex_sum . "\t100\n";
     echo "-----------------------end";
 }
 echo "\n\n\n";
 
 
-echo "Chain complex contact distribution\n";
+echo "// Chain complex contact distribution\n";
 if($asCSV){
     echo "Degree;Count;Percentage\n";
+    echo "0;" . $chain_complex_nulldeg . ";" . perc($chain_complex_sum, $chain_complex_nulldeg)  . "\n";
     for($i = 1; $i <= count($dist_chain_complex); $i++ ){
-        echo "" . $i . ";" . $dist_chain_complex[$i] . ";" . perc(array_sum($dist_chain_complex), $dist_chain_complex[$i]) . "\n";
-    }    
+        echo "" . $i . ";" . $dist_chain_complex[$i] . ";" . perc($chain_complex_sum, $dist_chain_complex[$i]) . "\n";
+    }   
+    echo "SUM;" . $chain_complex_sum . ";100\n";
 } else {
     echo "Degree\tCount\tPercentage\n";
+    echo "0\t" . $chain_complex_nulldeg . "\t" . perc($chain_complex_sum, $chain_complex_nulldeg) . "\n";
     for($i = 1; $i <= count($dist_chain_complex); $i++ ){
-        echo "" . $i . "\t" . $dist_chain_complex[$i] . "\t" . perc(array_sum($dist_chain_complex), $dist_chain_complex[$i]) . "\n";
+        echo "" . $i . "\t" . $dist_chain_complex[$i] . "\t" . perc($chain_complex_sum, $dist_chain_complex[$i]) . "\n";
     }
-
+    echo "\n";
+    echo "SUM\t" . $chain_complex_sum . "\t100\n";
     echo "-----------------------end";
 }
-
-
-
-
