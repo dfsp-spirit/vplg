4963bc3ee9d0418cf1400b37c97246b3c1f84543
Tim Schaefer <ts+code@rcmd.org>
2015-04-13 10:49:26 +0000

    added almost empty Netbeans project for Julian

---
+++ bk/bk_protsim/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: ts
+ *
+ * Created on April 13, 2015, 12:42 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hi there.\n";
+    return 0;
+}
+
