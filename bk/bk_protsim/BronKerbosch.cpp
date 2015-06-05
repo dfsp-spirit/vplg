6044dc1d9a24372be0037a321e9c1e9d964da57c
anonymous <anonymous@127.0.0.1>
2015-06-05 14:25:59 +0000

    redid the bk data structur to get away from std::set. greatly increased time performance

--- bk/bk_protsim/BronKerbosch.cpp
+++ bk/bk_protsim/BronKerbosch.cpp
@@ -7,24 +7,28 @@
 
 
 #include "BronKerbosch.h"
-int rec;
+
+
+
 /*
- * default constructor. does nothing.
+ * Constructor
  */
-BronKerbosch::BronKerbosch() {
-    
+BronKerbosch::BronKerbosch(const Graph_p& graph): g(graph), result(), T(), vertex_array(0), set_array(0) {
+    this->MAX_SETS = boost::num_vertices(this->g)*8;
+    this-> num_sets =0;
 }
 
 /*
- * default destructor. does nothing.
+ * Destructor
  */
 BronKerbosch::~BronKerbosch() {
-    
+    if (vertex_array) delete[] vertex_array;
+    if(set_array)       delete[] set_array;
 }
 
 /*
  * runs a modified Bron-Kerbosch algorithm on a boost::adjacency_list type graph
- * The edge_info struct has to contain a std::str attribute "label", containing either "z" or "u" for each edge to mark z-edges and u-edges. 
+ * The edge_info struct has to contain a std::string attribute "label", containing either "z" or "u" for each edge to mark z-edges and u-edges. 
  * The used algorithm uses an unspecified Pivot element and discriminates between z and u edges when building the cliques.
  * Returns a list of sets containing the VertexDescriptors of the vertices of the cliques.
  * Each set is a unique Clique in the graph.
@@ -32,47 +36,56 @@ BronKerbosch::~BronKerbosch() {
  * running it will lead to wrong results and in the worst case segfaults!!
  * The graph will only ever be read and never written to.
  */
-void BronKerbosch::run(const Graph_p& g) {
+void BronKerbosch::run() {
     /* 
      * this function works as an initializer for the recursive findCliques function.
      * It will start the findClique function n times for n vertices in the graph.
      * Each time it will pass a different, single vertex as the set C and its neighbours in the sets D, P or S.
      */
-    rec = 0;
-    this -> result.clear();
-    this -> T.clear();
     
-    std::set<VertexDescriptor_p> C;
+    this-> vertex_array = new object[boost::num_vertices(this->g)]();
+    this-> set_array = new object[MAX_SETS](); // 8 migth be to mutch must investigate further
     
+    int C = new_set();
     VertexIterator_p vi, ve;
-    for (boost::tie(vi,ve) = vertices(g); vi != ve; ++vi) { //iterate over all vertices in g
+    for (boost::tie(vi,ve) = boost::vertices(this->g); vi != ve; ++vi) { //iterate over all vertices in g
         std::cout << "[IN]    vertex " << *vi+1 <<" of " << *ve << "\n"; 
-        VertexDescriptor_p v = vertex(*vi,g);
-        C.insert(v);
-        std::set<VertexDescriptor_p> D;
-        std::set<VertexDescriptor_p> P;
-        std::set<VertexDescriptor_p> S;
+        VertexDescriptor_p v = boost::vertex(*vi, this->g);
+        insert_vertex(v,C);
+        int P = new_set();
+        int D = new_set();
+        int S = new_set();
         
         AdjacencyIterator_p ai, ae; //construct the neighbour set
-        for (boost::tie(ai,ae)=adjacent_vertices(v,g); ai!=ae;++ai) { //iterate over all neighbours of v
-            VertexDescriptor_p a = vertex(*ai,g);
+        for (boost::tie(ai,ae)=boost::adjacent_vertices(v, this->g); ai!=ae;++ai) { //iterate over all neighbours of v
+            VertexDescriptor_p a = boost::vertex(*ai, this->g);
             //if this neighbour is connected by a u-edge it is added to D, otherwise to S or P, 
             //if it was already used to initialize the algorithm once (if it is already in T) it will be added to S otherwise to P.
             //by adding already used vertices to S every Clique will be found exactly one and never multiple times.
-            if (zCon(v,a,g)) {
+            if (zCon(v,a)) {
                 if (T.find(a) != T.end()) {
-                    S.insert(a);
+                    insert_vertex(a,S);
                 } else { 
-                    P.insert(a);
+                    insert_vertex(a,P);
                 }
             } else {
-                D.insert(a);
+                insert_vertex(a,D);
             } //end else v and a z-connected
         }//end for all neighbours of v
-        findCliques(C, P, D, S, g); 
-        C.clear();
+        findCliques(C, P, D, S); 
+        remove_set(S);
+        remove_set(D);
+        remove_set(P);
+        remove_vertex(v,C);
+        
         this->T.insert(v);
     }//end for all vertices
+    
+    //clean up
+    remove_set(C);
+    this-> T.clear();
+    delete[] this->vertex_array; this->vertex_array = nullptr;
+    delete[] this->set_array;      this->set_array = nullptr;
 }// end run
 
 /*
@@ -82,13 +95,14 @@ void BronKerbosch::run(const Graph_p& g) {
  */
 void BronKerbosch::clear_results() {
     this->result.clear();
+    this->T.clear();
 }
 
 /*
  * Returns the complete list of all calculated Cliques.
  * If run(...) was not called before this function the result will be an empty list.
  */
-std::forward_list<std::set<VertexDescriptor_p>>  BronKerbosch::get_result_list() {
+std::forward_list<std::list<VertexDescriptor_p>>  BronKerbosch::get_result_list() {
         return this->result;
     }
 
@@ -98,11 +112,11 @@ std::forward_list<std::set<VertexDescriptor_p>>  BronKerbosch::get_result_list()
  */
  std::vector<int>  BronKerbosch::get_result_pattern() {
         std::vector<int> pattern;
-        for (std::set<VertexDescriptor_p> s : this->result) {
-            while (pattern.size() <= s.size() ) {
+        for (std::list<VertexDescriptor_p> c : this->result) {
+            while (pattern.size() <= c.size() ) {
                 pattern.push_back(0);
             }
-            ++pattern[s.size()];
+            ++pattern[c.size()];
         }
         return pattern;
     }
@@ -111,16 +125,16 @@ std::forward_list<std::set<VertexDescriptor_p>>  BronKerbosch::get_result_list()
  * Returns a list of all Cliques of maximum size
  * If run(...) was not called before this function the result will be an empty list.
  */
- std::forward_list<std::set<VertexDescriptor_p>> BronKerbosch::get_result_largest(){
-     std::forward_list<std::set<VertexDescriptor_p>> list;
+ std::forward_list<std::list<VertexDescriptor_p>> BronKerbosch::get_result_largest(){
+     std::forward_list<std::list<VertexDescriptor_p>> list;
      int largest = 0;
-     for (std::set<VertexDescriptor_p> s : this->result) {
-         if (s.size() > largest) {
-             largest = s.size();
+     for (std::list<VertexDescriptor_p> c : this->result) {
+         if (c.size() > largest) {
+             largest = c.size();
              list.clear();
          } else {
-             if (s.size() == largest) {
-                 list.push_front(s);
+             if (c.size() == largest) {
+                 list.push_front(c);
              }
          }
      }
@@ -133,88 +147,205 @@ std::forward_list<std::set<VertexDescriptor_p>>  BronKerbosch::get_result_list()
  * The function will then recursively find all Cliques containing this vertex.
  * All found cliques are added to the private member field result.
  */
-void BronKerbosch::findCliques(std::set<VertexDescriptor_p>& C, std::set<VertexDescriptor_p>& P, std::set<VertexDescriptor_p>& D,
-                                                     std::set<VertexDescriptor_p>& S, const Graph_p& g) {
-    if (P.empty() && S.empty()) {
-        this->result.push_front(C);//add the found clique to the result list
-        //std::cout << "[BK]    found Clique of size : " << C.size() <<" #-#-#\n";
+void BronKerbosch::findCliques(int C, int P, int D, int S) {
+    if (!set_array[P].next_vertex && !set_array[S].next_vertex) {
+        this->result.push_front(output_clique(C));//add the found clique to the result list
 
     } else {
-        std::set<VertexDescriptor_p> P1 = P;
-        std::set<VertexDescriptor_p> S1 = S;
+        int P1 = new_set(); copy_set(P,P1);
+        int S1 = new_set(); copy_set(S,S1);
         //chose a pivot element. Will choose the pivot from P, if possible, else from S.
         //no heuristic used, it is simply the first vertex in the set
         VertexDescriptor_p piv;
-        piv = P.empty()  ? *S.begin() : *P.begin();
+        object* os=set_array[S].next_vertex;
+        if( os ) piv=os->vertex; 
+        object* op=set_array[P].next_vertex;
+        if( op ) piv=op->vertex; 
+        
+        
         //start of main algorithm
         //iterates over all vertices connected to every vertex in C by a z-edge
-        
-        int count = 0;
-        int siz = P.size();
-        for (VertexDescriptor_p ui : P) {
-            std::cout << "[BK]        ";
-            for (int stuff = 0; stuff < rec; ++stuff) {std::cout << "    ";}
-            std::cout << "vertex " << ++count <<" of " << siz << "\n";
-            std::pair<EdgeDescriptor_p, bool> e = edge(piv, ui, g); 
+        while (op) {
+            VertexDescriptor_p ui = op->vertex;
+            std::pair<EdgeDescriptor_p, bool> e = boost::edge(piv, ui, this->g); 
             //check for edge between piv and ui. every vertex connected to piv can be skiped, as every Clique 
             //containing this vertex and the pivot have already been found
-            if ((!S.empty() &&!e.second) || (!S.empty() && zPath(piv, ui, D, g)) || S.empty()) {
+            if ((os && !e.second) || (os && zPath(piv, ui, D)) || !os) {
                 //create new sets for the next recursion
                 
-                std::set<VertexDescriptor_p> P2;
-                std::set<VertexDescriptor_p> D2;
-                std::set<VertexDescriptor_p> S2;
+                insert_vertex(ui, C);
+                int P2 = new_set();
+                int D2 = new_set();
+                int S2 = new_set();
                 
 
                 AdjacencyIterator_p ai, ae;
-                for (boost::tie(ai,ae)=adjacent_vertices(ui,g); ai!=ae;++ai) {
-                    VertexDescriptor_p v = vertex(*ai,g);
-                    //std::cout << "[BK]    current n : " << v << "\n"; 
-                    if (P1.find(v) != P1.end()) { //if v is in P
-                        P2.insert(v);
-                        //std::cout << "[BK]    v in p1 -> p2.insert\n"; 
+                for (boost::tie(ai,ae)=boost::adjacent_vertices(ui, this->g); ai!=ae;++ai) {
+                    VertexDescriptor_p n = boost::vertex(*ai, this->g);
+                    if (is_vertex_in_set(n,P1)) { //if n is in P
+                        insert_vertex(n, P2);
                     } else {
-                        if (D.find(v) != D.end()) { //if v is in D
-                            if (zCon(v,ui,g)) {//if c-edge between v and ui
-                                if (this->T.find(v) != this->T.end())   { S2.insert(v); /*std::cout << "[BK]    v in D, zcon, T -> S2.insert\n"; */} 
-                                else   { P2.insert(v); /*std::cout << "[BK]    v in D,  zcon -> P2.insert\n";*/}
-                            } else { D2.insert(v); /*std::cout << "[BK]    v in D, not zcon -> D2.insert\n";*/} // end if v and ui z-connected 
+                        if (is_vertex_in_set(n,D)) { //if n is in D
+                            if (zCon(n,ui)) {//if c-edge between n and ui
+                                if (this->T.find(n) == this->T.end())   {   
+                                    insert_vertex(n, P2);
+                                }  else {
+                                    insert_vertex(n, S2);
+                                } //end if else //if n is not in T
+                            } else {
+                                insert_vertex(n, D2); 
+                            } // end if else n and ui z-connected 
                         } else {
-                            if (S1.find(v) != S1.end())   { S2.insert(v); /*std::cout << "[BK]    v in not D, S1-> S2.insert\n";*/} 
-                        } //end if v  in D
-                    }//end if v in P
+                            if (is_vertex_in_set(n,S1))   {
+                                insert_vertex(n, S2);
+                            } //end if n in S1
+                        } //end if else n  in D
+                    }//end if else n in P
                 }// end for neigbours of ui
-                C.insert(ui);
-                //std::cout << "[BK]    before recursion\n";
-                //std::cout << "[BK]    recursion " << ++rec << "\n";
-                ++rec;
-                findCliques(C, P2, D2, S2, g);
-                --rec;
-                //std::cout << "[BK]    recursion " << --rec << "-----------------------\n";
-                //std::cout << "[BK]    after recursion\n";
-                C.erase(C.find(ui));
-                P1.erase(P1.find(ui));
-                S1.insert(ui);
+                findCliques(C, P2, D2, S2);
+                remove_set(S2);
+                remove_set(D2);
+                remove_set(P2);
+                remove_vertex(ui,C);
+                remove_vertex(ui,P1);
+                insert_vertex(ui,S1);
             }//end if ui adjacent to piv || z-path exists
+            op = op->next_vertex;
         }//end for vertices in P
+        remove_set(S1);
+        remove_set(P1);
     }//end if P and S not empty
 }// end findCliques()
 
 /*
  * returns true if the two vertices v and w are connected by a z marked edge in the graph g.
  */
-bool BronKerbosch::zCon(VertexDescriptor_p v, VertexDescriptor_p w, const Graph_p& g) {
+bool BronKerbosch::zCon(VertexDescriptor_p v, VertexDescriptor_p w) {
     EdgeDescriptor_p e; bool flag;
-    boost::tie(e, flag)= edge(v,w,g);
-    return flag && (g[e].label == "z");
+    boost::tie(e, flag)= boost::edge(v,w, this->g);
+    return flag && (this->g[e].label == "z");
 }//end zCon()
 
 /*
  * returns true if vertex start is neighbour of one vertex in set S, which is not connected to v
  */
-bool BronKerbosch::zPath(VertexDescriptor_p v, VertexDescriptor_p start, const std::set<VertexDescriptor_p>& S, const Graph_p& g) {
-    for (VertexDescriptor_p n : S) {
-        if (edge(start,n,g).second && n!=v) { return true;}
+bool BronKerbosch::zPath(VertexDescriptor_p v, VertexDescriptor_p start, int s) {
+    object* o = set_array[s].next_vertex;
+        while (o) {
+            VertexDescriptor n = o->vertex;
+            if (boost::edge(start,n, this->g).second && n!=v) { return true;}
+            o = o->next_vertex;
+        }
+        return false;
+}//end zPath()
+
+/*
+ * Deletes an object and frees the memory
+ */
+void BronKerbosch::remove_object(object* o) {
+    o->prev_set->next_set = o->next_set;
+    o->prev_vertex->next_vertex = o->next_vertex;
+    if( o->next_set )  { o->next_set->prev_set = o->prev_set; }
+    if( o->next_vertex ) { o->next_vertex->prev_vertex = o->prev_vertex; }
+    delete o;
+}
+
+/*
+ * Deletes all objects associated to one set and invalidates the pointer to that chain
+ */
+void BronKerbosch::remove_set(int s){
+    if (s != this->num_sets) {
+        std::cerr << "not last set\n";
+        exit(1); 
+    }
+    while (set_array[s].next_vertex) {
+        remove_object(set_array[s].next_vertex);
+    }
+    set_array[s].next_vertex = nullptr;
+    --(this->num_sets);
+}
+    
+/*
+ * removes a vertex v from set s
+ */
+void BronKerbosch::remove_vertex(int v, int s){
+    object* o= vertex_array[v].next_set;
+    if( !o || o->set != s ) {
+        std::cerr << " not in last set\n";
+        exit(1);
+    }
+    remove_object(o);
+}
+    
+/*
+ * Copies the contents from set s1 to s2. s1 remains unmodified.
+ */
+void BronKerbosch::copy_set(int s1, int s2){
+    object* o = set_array[s1].next_vertex;
+    while( o ) {
+        insert_vertex(o->vertex,s2);
+        o = o->next_vertex;
+    }
+}
+    
+/*
+ * creates a new object and inserts it into the pointer chains starting at vertex_array[v] and set_array[s]
+ */
+void BronKerbosch::insert_vertex(int v, int s){
+    object *o = new object();
+    //set the values of the new object
+    o->vertex = v;
+    o->set = s;
+    //set the pointers to the the dummy objects in the arrays
+    o->prev_vertex = &set_array[s];
+    o->prev_set = &vertex_array[v];
+    //set the pointers to the next elements in the object chains
+    o->next_vertex = set_array[s].next_vertex;
+    o->next_set = vertex_array[v].next_set;
+    //set the pointers of the dummy objects in the arrays to point to this object
+    vertex_array[v].next_set = o;
+    set_array[s].next_vertex = o;
+    //if there are objects in the chains (the new object is not the only one)
+    // set the previous pointers of those objects to this object
+    if( o->next_vertex ) {
+        o->next_vertex->prev_vertex = o;}
+    if( o->next_set ) {
+        o->next_set->prev_set = o;}
+}
+    
+/*
+ * returns true if vertex v is found in the set s
+ */
+bool BronKerbosch::is_vertex_in_set(int v, int s){
+    object* o = vertex_array[v].next_set;
+    for (int i = 0; (i <= 8) && o; ++i) {
+        if (o->set == s) {
+            return true;
+        }
+        o = o->next_set;
     }
     return false;
-}//end zPath()
+}
+    
+/*
+ * Returns the index of a new, empty set.
+ */
+int BronKerbosch::new_set(){
+    if (this->num_sets > this->MAX_SETS) {
+        std::cerr << "Reached maximum sets"; exit(1);
+    }
+    return ++(this->num_sets);
+}
+    
+/*
+ *  writes all vertices from a set to a std::list
+ */
+std::list<VertexDescriptor_p> BronKerbosch::output_clique(int s) {
+    std::list<VertexDescriptor_p> res;
+    object* o = set_array[s].next_vertex;
+    while (o) {
+        res.push_front(o->vertex);
+        o = o->next_vertex;
+    }
+    return res;
+}
--- bk/bk_protsim/BronKerbosch.h
+++ bk/bk_protsim/BronKerbosch.h
@@ -13,6 +13,19 @@
 #include <forward_list>
 
 /*
+ * struct to handle the membership of vertices to sets. 
+ */
+struct object {
+    object(): vertex(0), set(0), next_set(0),prev_set(0), next_vertex(0), prev_vertex(0){}
+    unsigned long vertex; //to be compatible with VertexDescptor (typename for unsigned long)
+    int set;
+    object* next_set;
+    object* prev_set;
+    object* next_vertex;
+    object* prev_vertex;
+};
+
+/*
  * This class finds all Cliques in a Graph using a modified version of the Bron-Kerbosch algorithm.
  * To do so simply pass a boost::adjacency_list type Graph to the run(...) method.
  * The result of the search will be stored in the class object and can be accessed by the get_result_...() functions.
@@ -21,24 +34,44 @@
  */
 class BronKerbosch {
 public:
-    BronKerbosch();
+    BronKerbosch(const Graph_p& graph);
     ~BronKerbosch();
     
-    void  run(const Graph_p& g);
+    void  run();
     void clear_results();
-    std::forward_list<std::set<VertexDescriptor_p>>  get_result_list();
+    std::forward_list<std::list<VertexDescriptor_p>>  get_result_list();
     std::vector<int>  get_result_pattern();
-    std::forward_list<std::set<VertexDescriptor_p>> get_result_largest();
+    std::forward_list<std::list<VertexDescriptor_p>> get_result_largest();
     
     
 private:
-    std::forward_list<std::set<VertexDescriptor_p>> result;
+    //members needed to find the cliques in a recursive function (work like global variables).
+    const Graph_p& g;
+    int num_sets;
+    int MAX_SETS;
+    object* set_array;
+    object* vertex_array;
     std::set<VertexDescriptor_p> T;
     
-    void findCliques(std::set<VertexDescriptor_p>& C, std::set<VertexDescriptor_p>& P, std::set<VertexDescriptor_p>& D,
-                                 std::set<VertexDescriptor_p>& S, const Graph_p& g);
-    bool zPath(VertexDescriptor_p v, VertexDescriptor_p w, const std::set<VertexDescriptor_p>& s, const Graph_p& g);
-    bool zCon(VertexDescriptor_p v, VertexDescriptor_p w, const Graph_p& g);
+    //stores the results 
+    std::forward_list<std::list<VertexDescriptor_p>> result;
+    
+    //convertes the results from the internal format into a std container
+    std::list<VertexDescriptor_p> output_clique(int s);
+    
+    //main function for calculation
+    void findCliques(int C, int P, int D, int S);
+    bool zPath(VertexDescriptor_p v, VertexDescriptor_p w, int s);
+    bool zCon(VertexDescriptor_p v, VertexDescriptor_p w);
+    
+    //functions to handle the data
+    void remove_object(object* o);
+    void remove_set(int s);
+    void remove_vertex(int v, int s);
+    void copy_set(int s1, int s2);
+    void insert_vertex(int v, int s);
+    bool is_vertex_in_set(int v, int s);
+    int new_set();
 };
         
 
