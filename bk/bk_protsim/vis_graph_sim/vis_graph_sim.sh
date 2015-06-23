da205815bb0659bfb05c59ee7db775ebfc4c34d9
Tim Schaefer <ts+code@rcmd.org>
2015-06-23 15:16:33 +0000

    working on visual comparison of graphs

--- bk/bk_protsim/vis_graph_sim/vis_graph_sim.sh
+++ bk/bk_protsim/vis_graph_sim/vis_graph_sim.sh
@@ -4,8 +4,23 @@ FIRST_GRAPH="$1"
 SECOND_GRAPH="$2"
 
 
-./bk_protsim "$FIRST_GRAPH" "$SECOND_GRAPH" -l -f
-java -jar plcc.jar NONE --draw-gml-graph "$FIRST_GRAPH" results_0_first.txt
-java -jar plcc.jar NONE --draw-gml-graph "$SECOND_GRAPH" results_0_second.txt
+BK="./bk_protsim"
+PLCC="plcc.jar"
+
+APPTAG="VGS "
+
+if [ ! -f "$BK" ]; then
+  echo "$APPTAG Missing bk_protsim binary"
+  exit 1
+fi
+
+if [ ! -f "$PLCC" ]; then
+  echo "$APPTAG Missing bk_protsim binary"
+  exit 1
+fi
+
+$BK "$FIRST_GRAPH" "$SECOND_GRAPH" -l -f
+java -jar $PLCC NONE --draw-gml-graph "$FIRST_GRAPH" results_0_first.txt
+java -jar $PLCC NONE --draw-gml-graph "$SECOND_GRAPH" results_0_second.txt
 
 echo "Done. Check files '$FIRST_GRAPH.png' and '$SECOND_GRAPH.png'."
