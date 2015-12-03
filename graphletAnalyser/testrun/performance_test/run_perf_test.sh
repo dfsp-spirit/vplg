0f39d6c3a14190f8aee20e912281e0aaa132d1fb
Tim Schäfer <dfsp-spirit@users.noreply.github.com>
2015-12-03 18:41:13 +0100

    adapt GA performance test script

--- graphletAnalyser/testrun/performance_test/run_perf_test.sh
+++ graphletAnalyser/testrun/performance_test/run_perf_test.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+echo "===PG tests==="
 for FILE in *_PG.gml; do echo "Running GA for PG file '$FILE'..."; ./graphletanalyser --sse_graph --silent --timerun $FILE; done
+
+
+echo "===CG tests==="
+for FILE in *_CG.gml; do echo "Running GA for CG file '$FILE'..."; ./graphletanalyser --complex_graph --silent --timerun $FILE; done
+
+
+echo "===AAG tests==="
+for FILE in *_aagraph.gml; do echo "Running GA for AAG file '$FILE'..."; ./graphletanalyser --aa_graph --silent --timerun $FILE; done
+
