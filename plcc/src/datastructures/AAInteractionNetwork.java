95ccc8b9425c722acdb938f522f198367ab09f74
Tim Schäfer <dfsp-spirit@users.noreply.github.com>
2015-12-22 19:06:19 +0100

    add new class for AAI

---
+++ plcc/src/datastructures/AAInteractionNetwork.java
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the Visualization of Protein Ligand Graphs (VPLG) software package.
+ *
+ * Copyright Tim Schäfer 2015. VPLG is free software, see the LICENSE and README files for details.
+ *
+ * @author ts
+ */
+package datastructures;
+
+import java.util.List;
+
+/**
+ * Simple wrapper to turn the AA matrix into a SimpleGraphInterface, allowing us to compute graph properties on it. Use AminoAcid.names3 to get the list of strings representing the vertex types.
+ * Atm this makes little sense because the matrix is usually complete for proteins, i.e., each AA type interacts with each other type. The weights are of more interest.
+ * @author spirit
+ */
+public class AAInteractionNetwork extends SparseGraph<String, Integer>{
+            
+    public AAInteractionNetwork(List<String> vertices, int[][] edges) {
+        super(vertices);
+        for(int i = 0; i < edges.length; i++) {
+            for(int j = 0; j < edges[0].length; j++) {
+                this.addEdge(i, j, edges[i][j]);
+            }
+        }
+        
+    }
+    
+}
