1b7f5b279a02e00c94ce1a8e4a28fabb47eb81e5
Tim Schäfer <dfsp-spirit@users.noreply.github.com>
2016-05-02 22:21:50 +0200

    minor change in simple PDB file download script

--- plcc/testrun/get_pdb_and_dssp_file_from_web.sh
+++ plcc/testrun/get_pdb_and_dssp_file_from_web.sh
@@ -4,7 +4,8 @@ PDBID="$1"
 
 echo "Downloading PDB file for ${PDBID}..."
 wget http://www.rcsb.org/pdb/files/${PDBID}.pdb.gz
-tar xzf ${PDBID}.pdb.gz
+#tar xzf ${PDBID}.pdb.gz
+gunzip ${PDBID}.pdb.gz
 
 echo "Downloading DSSP file for ${PDBID}..."
 wget ftp://ftp.cmbi.ru.nl/pub/molbio/data/dssp/ ${PDBID}.dssp
