de6446ec6aa671bdced44fd3f8adffce0a83411d
Tim Schäfer <dfsp-spirit@users.noreply.github.com>
2016-07-18 13:19:27 +0200

    update JAR, build script output

--- plcc/build-plcc.sh
+++ plcc/build-plcc.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+APPTAG="[BPLCC]"
+echo "$APPTAG Remember that you need JUnit for this to work. Running 'ant ci'..."
 ant ci
+echo "$APPTAG If no error showed up, the binary should be in 'store/plcc.jar'."
 
