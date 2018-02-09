7dd781dca1e97f972eddc157387efdc288c2bbb1
JNWolf <JNWolfUni@web.de>
2018-02-09 11:07:49 +0100

    Change create filelist script: new modes added

--- plcc/tools/plcc_cluster/create_host_filelists.sh
+++ plcc/tools/plcc_cluster/create_host_filelists.sh
@@ -1,6 +1,37 @@
 #!/bin/sh
 ## create_host_filelists.sh -- create the file lists for all nodes. You should have the hostnames (DNS names, e.g., "worker1.cluster.mydomain.com" or "worker1") in a file named 'settings_node_hostnames.cfg' (one name per line) before running this script.
+# written by ts
+# changed by jnw 2017: include a mode without timestamp file, include use of debug mode (see cluster settings)
+# 		 2018: include a mode with provided filelist 
+#
+# USAGE
+# parameter:
+# '' -> incremental update with timestamp file
+# 'YYYY-MM-DD' -> incremental update without timestamp file. date of last update start
+# 'all' -> update everything
+# 'filelist' -> expects ./status/dbinsert_file_list.lst with path to pdb files each per line and creates filelists for each node (as usual)
 
+########## Some function(s) ##########
+
+# from: https://wiki.magenbrot.net/programmierung/bash/datum-zeit_berechnung
+# calculates the difference between two dates
+# -* gives difference in seconds (s), minutes (m), hours (h), days (d) [default]
+# next two parameters are dates in format "YYYY-MM-DD"
+# function needed in workaround for missing timestamp /jnw
+dateDiff (){
+    case $1 in
+        -s)   sec=1;      shift;;
+        -m)   sec=60;     shift;;
+        -h)   sec=3600;   shift;;
+        -d)   sec=86400;  shift;;
+        *)    sec=86400;;
+    esac
+    dte1=$(date --utc --date "$1" +%s)
+    dte2=$(date --utc --date "$2" +%s)
+    diffSec=$((dte2-dte1))
+    if ((diffSec < 0)); then abs=-1; else abs=1; fi
+    echo $((diffSec/sec*abs))
+}
 
 
 ########## Settings ##########
@@ -104,10 +135,11 @@ fi
 
 echo "$APPTAG Using PDB files in sub directories of '$LOCAL_MIRRORDIR'. Assuming file extension '$REMOTE_PDB_FILE_EXTENSION'."
 
-if [ "$1" = "all" ]; then
-    DATE_FILTER=""
-    echo "$APPTAG Updating all proteins in the database, even if they did not change more recently than the last update."
-else
+# included workaround for missing timestamp-file /jnw
+ASUME_FILELIST="false"
+if [ -z "$1" ]; then # => incremental update using timestamp file
+    echo "$APPTAG No given parameter: Trying incremental update using timestamp file."
+    
     if [ ! -r $TIMESTAMP_FILE_LAST_UPDATE_START ]; then
         echo "$APPTAG ##### ERROR: Requested partly update but no timestamp file found at '$TIMESTAMP_FILE_LAST_UPDATE_START'. #####"
         echo "$APPTAG This most likely means that this is the 1st time this is run. Use '$0 all' to update all files."
@@ -117,16 +149,65 @@ else
     LAST_UPDATE_DATE=$(stat -c %y $TIMESTAMP_FILE_LAST_UPDATE_START)
     echo "$APPTAG Last update was started at $LAST_UPDATE_DATE."
     DATE_FILTER="-cnewer $TIMESTAMP_FILE_LAST_UPDATE_START"
-
+else
+    case "$1" in
+        "all") # => updating everything
+            echo "$APPTAG Given parameter 'all': Updating all proteins in the database, even if they did not change more recently than the last update."
+            DATE_FILTER=""
+            ;;
+        "filelist") # expecting a existing list with paths
+            ASUME_FILELIST="true"
+            ;;
+        *) # => incremental update using given date as last update date
+            echo "$APPTAG Given parameter '$1': Trying incremental update using $1 as last update date."
+        
+            # check length of parameter
+            if [ $(expr length "$1") != 10 ]; then
+                echo "$APPTAG Error: It seems like you tried to use incremental update without timestamp file."
+                echo "$APPTAG Error: Make sure your parameter is of format YYYY-MM-DD (length did not match) and rerun script. Exiting now."
+                exit 1
+            fi
+            
+            # TODO check for correctness of given parameter in YYYY-MM-DD, for example check wether YYYY is year of date $1, MM is month of date $1 ...
+            
+            echo "$APPTAG Make absolutely sure that the parameter follows the convention 'YYYY-MM-DD'! It is not checked!"
+            echo "$APPTAG Otherwise the resulting filelists may be wrong. Check for plausibility! Atleast the lists should not be empty."
+            
+            LAST_UPDATE_DATE=$1
+            
+            DATE_DIFF=$(dateDiff $LAST_UPDATE_DATE $(date +%Y-%m-%d))
+            DATE_FILTER="-ctime -$DATE_DIFF"
+            ;;
+    esac
 fi
+
+echo "$APPTAG Using Datefilter: '$DATE_FILTER'"
+
+# end of workaround /jnw
 
 ## Write the list of files we need to handle
 echo "$APPTAG Searching for PDB files that need to be handled..."
-find $LOCAL_MIRRORDIR/ $DATE_FILTER -type f -name *$REMOTE_PDB_FILE_EXTENSION > $DBINSERT_FILE_LIST
+
+# find PDB files (check for DEBUG mode and existence of filelist)
+if [ $ASUME_FILELIST = "true" ]; then
+    if [ ! -r $DBINSERT_FILE_LIST ]; then
+        echo "$APPTAG Requested creation of node filelists using a existing filelist at $DBINSERT_FILE_LIST but no list found."
+        echo "$APPTAG Exiting now."
+        exit 1  
+    fi
+else
+    if [ $DEBUG ]; then
+        if [ -z $NUM_DEBUG ]; then
+            echo "$APPTAG Error: Debug mode set true but no DEBUG_Num defined"
+        else
+            find $LOCAL_MIRRORDIR/ $DATE_FILTER -type f -name *$REMOTE_PDB_FILE_EXTENSION | head -$NUM_DEBUG > $DBINSERT_FILE_LIST
+        fi
+    else # act normal
+        find $LOCAL_MIRRORDIR/ $DATE_FILTER -type f -name *$REMOTE_PDB_FILE_EXTENSION > $DBINSERT_FILE_LIST
+    fi
+fi
 
 NUM_FILES=$(cat $DBINSERT_FILE_LIST | wc -w)
-
-
 
 echo "$APPTAG Found $NUM_FILES PDB files that need to be handled. Wrote unprocessed list to '$DBINSERT_FILE_LIST'."
 
