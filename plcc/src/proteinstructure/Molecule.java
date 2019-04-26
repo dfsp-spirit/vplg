f15ebf2b1ad058b37b033a57b138b7d79cf90b3d
kkorolew <kkorolew@web.de>
2019-04-26 09:42:45 +0200

    Add methods getAlphaCarbonAtom, hasAtom, isLigand, isAA, isOtherRes, isRNA to Molecule class

--- plcc/src/proteinstructure/Molecule.java
+++ plcc/src/proteinstructure/Molecule.java
@@ -8,7 +8,7 @@
 package proteinstructure;
 
 import java.util.ArrayList;
-
+import proteinstructure.SSE;
 /**
  *
  * @author kk
@@ -33,4 +33,35 @@ public class Molecule {
     private String chainID = null;
     private String modelID = null;
     private String iCode = null; 
+    
+   
+     /**
+     * Returns the C alpha atoms of this residue or null if it has none.
+     * @return the alpha carbon or null 
+     */
+    public Atom getAlphaCarbonAtom() {
+        if(this.isAA()) {
+            for(Atom a : this.atoms) {
+                if(a.isCalphaAtom()) {
+                    return a;
+                }
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Determines whether this Residue has at least one Atom.
+     * @return true if this Residue has at least one Atom, false otherwise.
+     */ 
+    public boolean hasAtoms() {
+        return(this.atoms.size() > 0);
+    }
+    
+    public Boolean isLigand() { return(this.type.equals(Residue.RESIDUE_TYPE_LIGAND)); }
+    public Boolean isAA() { return(this.type.equals(Residue.RESIDUE_TYPE_AA)); }
+    public Boolean isOtherRes() { return(this.type.equals(Residue.RESIDUE_TYPE_OTHER)); }
+    public Boolean isRNA() { return(this.type.equals(Residue.RESIDUE_TYPE_RNA)); }
+
+   
 }
