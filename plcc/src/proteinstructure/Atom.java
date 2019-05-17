241eb0f65aa7bdcc33d45250a58ed849ab54aaa9
kkorolew <kkorolew@web.de>
2019-05-17 11:35:48 +0200

    Change vars

--- plcc/src/proteinstructure/Atom.java
+++ plcc/src/proteinstructure/Atom.java
@@ -44,6 +44,7 @@ public class Atom implements java.io.Serializable {
     public static final Integer ATOMTYPE_LIGAND = 1;
     public static final Integer ATOMTYPE_IGNORED_LIGAND = 2;
     public static final Integer ATOMTYPE_IGNORED_ATOM = 3;
+    public static final Integer ATOMTYPE_RNA = 4;
 
     /**
      * Getter for PDB alternate location identifier.
