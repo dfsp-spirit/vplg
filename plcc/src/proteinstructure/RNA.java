431a79dd41cfc946698566222c25a712b6f8b123
kkorolew <kkorolew@web.de>
2019-05-17 11:36:59 +0200

    Add constructor

--- plcc/src/proteinstructure/RNA.java
+++ plcc/src/proteinstructure/RNA.java
@@ -20,6 +20,24 @@ import tools.DP;
  * @author katja
  */
 public class RNA extends Molecule implements java.io.Serializable {
+    public RNA() {} //Standardkonstruktor
+    public RNA (Molecule Mol) {
+        this.atoms = Mol.getAtoms();
+        this.chain = Mol.getChain();
+        this.chainID = Mol.getChainID();
+        this.modelID = Mol.getModelID();
+        this.iCode = Mol.getiCode();
+        this.centerSphereRadius = Mol.getCenterSphereRadius();
+        this.pdbNum = Mol.getPdbNum();
+        this.dsspNum = Mol.getDsspNum();
+        this.Name3 = Mol.getName3();
+        this.AAName1 = Mol.getAAName1();
+        this.sseString = Mol.getSSEString();
+        this.sse = Mol.getSSE();
+        this.sseStringDssp = Mol.getSSEStringDssp();
+        this.isPartOfDsspSse = Mol.getDsspSseState();
+    }
+    
 
    
     
