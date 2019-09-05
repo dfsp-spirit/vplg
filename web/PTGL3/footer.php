195d8a18d62e50a02aa64c080586c0bbd62307b4
JNWolf <JNWolfUni@web.de>
2019-09-05 15:37:58 +0200

    Update footer on website

--- web/PTGL3/footer.php
+++ web/PTGL3/footer.php
@@ -1,19 +1,19 @@
 <footer id="footer">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-2">
+			<div class="col-sm-1">
 				<a href="about.php" class="footerLink"><big>About</big></a>
 			</div>
 
-			<div class="col-sm-2">
+			<div class="col-sm-1">
 				<a class="footerLink" href="imprint.php"><big>Imprint</big></a>
 			</div>
 		
-			<div class="col-sm-2">
+			<div class="col-sm-1">
 				<a class="footerLink" href="help.php"><big>Help</big></a>
 			</div>
 			
-			<div class="col-sm-2">
+			<div class="col-sm-1">
 				<a class="footerLink" href="contact.php"><big>Contact</big></a>
 			</div>
 			
@@ -21,16 +21,20 @@
 				<a class="footerLink" href="http://www.bioinformatik.uni-frankfurt.de" target="_blank"><big>MolBI Group</big></a>
 			</div>
 			
-			<div class="col-sm-2">
+			<div class="col-sm-1">
 				<a class="footerLink" href="publications.php"><big>Publications</big></a>
 			</div>
 
-			<div class="col-sm-2">
+			<div class="col-sm-1">
 				<a href="citing.php" class="footerLink"><big>Citing</big></a>
 			</div>
 			
 			<div class="col-sm-2">
 				<a href="about.php#linking" class="footerLink"><big>Linking PTGL</big></a>
+			</div>
+
+			<div class="col-sm-2">
+				<a href="http://www.uni-frankfurt.de/70876599/Datenschutzerklaerung" class="footerLink"><big>Privacy Policy</big></a>
 			</div>
 			
 			<!--
