514e88bee96e7a6b86c30e661779bad606a2a9a9
JNWolf <JNWolfUni@web.de>
2019-11-13 13:18:54 +0100

    Add classes for dissolved file parsers

---
+++ plcc/src/io/LegacyParser.java
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the Visualization of Protein Ligand Graphs (VPLG) software package.
+ *
+ * Copyright Tim Schäfer 2012. VPLG is free software, see the LICENSE and README files for details.
+ *
+ * @author ts
+ */
+
+package io;
+
+/**
+ *
+ * @author jnw
+ */
+public class LegacyParser extends FileParser {
+    
+}
