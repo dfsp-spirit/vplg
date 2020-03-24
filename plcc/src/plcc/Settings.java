60aa4e790dab956c58ba405e8b286cab3762a1ee
Selina <Selina@Drogon.fritz.box>
2020-03-24 14:00:17 +0100

    new Setting for filling gaps between two strands

--- plcc/src/plcc/Settings.java
+++ plcc/src/plcc/Settings.java
@@ -435,6 +435,7 @@ public class Settings {
         defSet("plcc_S_ligSSECode", "L", "The SSE code used to mark a SSE of type ligand");
         defSet("plcc_B_include_coils", "false", "Whether coils (DSSP SSE type ' ') should be considered");
         defSet("plcc_B_change_dssp_sse_b_to_e", "false", "Whether all isolated beta bridges (DSSP SSE type: B) should be changed to betastrands (DSSP SSE type: E)");
+        defSet("plcc_B_fill_gaps", "false", "Whether two strands should be fused to one strand, when there is only one AA (with DSSP SSE ' ') between them.");
         defSet("plcc_S_coilSSECode", "C", "The amino acid code used to mark a coiled region residue");
         defSet("plcc_S_ligAACode", "J", "The amino acid code used to mark a ligand residue");
         defSet("plcc_B_uglySQLhacks", "false", "Whether to rewrite string data like ligand atom formulas before inserting into DB");
