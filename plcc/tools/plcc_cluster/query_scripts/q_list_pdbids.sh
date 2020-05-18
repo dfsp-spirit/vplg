d6dcb184f63f3cc50d8072d73d4d5f98717db910
JNWolf <JNWolfUni@web.de>
2020-05-18 16:08:54 +0200

    Extend script to write lists of large and non-large IDs of DB

--- plcc/tools/plcc_cluster/query_scripts/q_list_pdbids.sh
+++ plcc/tools/plcc_cluster/query_scripts/q_list_pdbids.sh
@@ -13,6 +13,12 @@ psql $PSQL_OPTIONS -c 'SELECT DISTINCT pdb_id FROM plcc_chain;' | sed 's/^ //' >
 echo "Creating list of successfully inserted PDB IDs (insert_completed = t)."
 psql $PSQL_OPTIONS -c "SELECT pdb_id FROM plcc_protein WHERE insert_completed = 't';" | sed 's/^ //' > ./results/list_successful_pdbids.txt
 
+echo "Creating list of successfully inserted non-large PDB IDs (insert_completed = t)."
+psql $PSQL_OPTIONS -c "SELECT pdb_id FROM plcc_protein WHERE insert_completed = 't' AND is_large_structure = 'f';" | sed 's/^ //' > ./results/list_successful_non-large_pdbids.txt
+
+echo "Creating list of successfully inserted large PDB IDs (insert_completed = t)."
+psql $PSQL_OPTIONS -c "SELECT pdb_id FROM plcc_protein WHERE insert_completed = 't' AND is_large_structure = 't';" | sed 's/^ //' > ./results/list_successful_large_pdbids.txt
+
 echo "Creating list of aborted PDB IDs (insert_completed = f)."
 psql $PSQL_OPTIONS -c "SELECT pdb_id FROM plcc_protein WHERE insert_completed = 'f';" | sed 's/^ //' > ./results/list_aborted_pdbids.txt
 
