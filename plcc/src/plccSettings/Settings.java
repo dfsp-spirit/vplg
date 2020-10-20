e90675475f183465b79bcd6d8cbe57f9b1e3cd91
JNWolf <JNWolfUni@web.de>
2020-10-20 16:14:23 +0200

    Change to static init and add setting->section map

--- plcc/src/plccSettings/Settings.java
+++ plcc/src/plccSettings/Settings.java
@@ -20,57 +20,77 @@ import tools.DP;
  * @author jnw
  */
 public class Settings {
-    static ArrayList<Section> sections = initSections();  // holds the setting sections in their correct order
-    static HashMap<String, String> mapSettingsSection;  // maps the settings names (=keys) to the section they are in
+    static ArrayList<Section> sections;  // holds the setting sections in their correct order
+    static HashMap<String, String> mapSettingsSection;  // WARNING: MUST be filled after sections. Maps the settings names (=keys) to the section they are in
     
     static final String PACKAGE_TAG = "SETTINGS";
     static final String FS = System.getProperty("file.separator");
     static private final File DEFAULT_FILE = new File(System.getProperty("user.home") + FS  + "plcc_settings.txt");
+    
+    // init all the static fields
+    static {
+        // first, init sections
+        initSections();
+        
+        // second, init map
+        initMap();
+    }
 
     /**
      * Creates all the settings sections.
      * @return settings sections.
      */
-    static private ArrayList<Section> initSections() {
-        ArrayList<Section> tmpSections = new ArrayList<>();
+    static private void initSections() {
+        sections = new ArrayList<>();
         
         // add new sections here
         // USER SECTIONS
-        tmpSections.add(new Section("General settings", "User"));
-        tmpSections.add(new Section("Amino Acid Graphs (AAG)", "User"));
-        tmpSections.add(new Section("Protein Graph (PG)", "User"));
-        tmpSections.add(new Section("Graph types", "User"));
-        tmpSections.add(new Section("Folding Graph (FG)", "User"));
-        tmpSections.add(new Section("Complex Graph (CG)", "User"));
-        tmpSections.add(new Section("Structure visualization", "User"));
+        sections.add(new Section("General settings", "User"));
+        sections.add(new Section("Amino Acid Graphs (AAG)", "User"));
+        sections.add(new Section("Protein Graph (PG)", "User"));
+        sections.add(new Section("Graph types", "User"));
+        sections.add(new Section("Folding Graph (FG)", "User"));
+        sections.add(new Section("Complex Graph (CG)", "User"));
+        sections.add(new Section("Structure visualization", "User"));
         
         // ADVANCED SECTIONS
-        tmpSections.add(new Section("Prints / Error handling", "Advanced"));
-        tmpSections.add(new Section("Performance", "Advanced"));
-        tmpSections.add(new Section("Parser", "Advanced"));
-        tmpSections.add(new Section("Secondary Structure Element (SSE)", "Advanced"));
-        tmpSections.add(new Section("SSE orientation", "Advanced"));
-        tmpSections.add(new Section("Contact definition", "Advanced"));
-        tmpSections.add(new Section("AAG", "Advanced"));
-        tmpSections.add(new Section("Database (DB) connection", "Advanced"));
-        tmpSections.add(new Section("DB settings", "Advanced"));
-        tmpSections.add(new Section("DB structure search", "Advanced"));
-        tmpSections.add(new Section("FG", "Advanced"));
-        tmpSections.add(new Section("PG", "Advanced"));
-        tmpSections.add(new Section("CG", "Advanced"));
-        tmpSections.add(new Section("Motifs", "Advanced"));
-        tmpSections.add(new Section("Cluster mode", "Advanced"));
-        tmpSections.add(new Section("DB: representative chains", "Advanced"));
-        tmpSections.add(new Section("Image settings", "Advanced"));
-        tmpSections.add(new Section("Output settings", "Advanced"));
-        tmpSections.add(new Section("DB: similarity search", "Advanced"));        
+        sections.add(new Section("Prints / Error handling", "Advanced"));
+        sections.add(new Section("Performance", "Advanced"));
+        sections.add(new Section("Parser", "Advanced"));
+        sections.add(new Section("Secondary Structure Element (SSE)", "Advanced"));
+        sections.add(new Section("SSE orientation", "Advanced"));
+        sections.add(new Section("Contact definition", "Advanced"));
+        sections.add(new Section("AAG", "Advanced"));
+        sections.add(new Section("Database (DB) connection", "Advanced"));
+        sections.add(new Section("DB settings", "Advanced"));
+        sections.add(new Section("DB structure search", "Advanced"));
+        sections.add(new Section("FG", "Advanced"));
+        sections.add(new Section("PG", "Advanced"));
+        sections.add(new Section("CG", "Advanced"));
+        sections.add(new Section("Motifs", "Advanced"));
+        sections.add(new Section("Cluster mode", "Advanced"));
+        sections.add(new Section("DB: representative chains", "Advanced"));
+        sections.add(new Section("Image settings", "Advanced"));
+        sections.add(new Section("Output settings", "Advanced"));
+        sections.add(new Section("DB: similarity search", "Advanced"));        
         
         // DEVELOPER SECTIONS
-        tmpSections.add(new Section("Debug", "Developer"));
-        tmpSections.add(new Section("Disabled", "Developer"));
-        tmpSections.add(new Section("Alternate AAG", "Developer"));
-        
-        return tmpSections;
+        sections.add(new Section("Debug", "Developer"));
+        sections.add(new Section("Disabled", "Developer"));
+        sections.add(new Section("Alternate AAG", "Developer"));
+    }
+    
+    
+    /**
+     * Creates a map settings names to section names. Always call after sections are created!
+     */
+    static private void initMap() {
+        mapSettingsSection = new HashMap<>();
+        for (Section tmpSection : sections) {
+            for (Setting tmpSetting : tmpSection.settings) {
+                mapSettingsSection.put(tmpSetting.name, tmpSection.name);
+            }
+        }
     }
     
     
@@ -78,9 +98,11 @@ public class Settings {
         // read settings in if existing and create otherwise
         if (io.IO.fileExistsIsFileAndCanRead(DEFAULT_FILE)) {
             Properties readProperties = io.IO.readPropertiesFromFile(DEFAULT_FILE.getAbsolutePath());
+            @SuppressWarnings("unchecked")  // skip warning associated to next line, according to https://www.boraji.com/how-to-iterate-properites-in-java
             Enumeration<String> enumer = (Enumeration<String>) readProperties.propertyNames();  // seems we have to create it outside the loop head
             while (enumer.hasMoreElements()) {
                 // TODO set values
+                break;
             }
         } else {
             // create default settings file
